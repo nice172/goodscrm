9cfe6baa541c3bca3374faf5762cc2cda77baff3
liaoyinzong <354575573@qq.com>
2018-09-04 11:11:22 +0800

    no message

--- application/admin/config.php
+++ application/admin/config.php
@@ -6,7 +6,15 @@ return [
         'NO_AUTH_USER' => array(1,2),
         'AUTH_ON' => true,
         'AUTH_USER' => config('database.prefix').'users',
-        'NO_AUTH_URL' => array(),
+        'NO_AUTH_URL' => array(
+            'admin/users/check_nick','admin/users/check_name',
+            'admin/order/create_do','admin/customers/add_do',
+            'admin/customers/edit_do','admin/contacts/add_do',
+            'admin/contacts/edit_do','admin/contacts/check_name',
+            'admin/purchase/edit_do','admin/supplier/add_do',
+            'admin/supplier/edit_do','admin/supplier/contacts_do',
+            'admin/supplier/edit_e_do'
+        ),
     ),
     'PAGE_SIZE' => 10,
     'UPLOAD_DIR' => 'uploads'
