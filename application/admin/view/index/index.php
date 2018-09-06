9cbf692ac087e14516ac204c78e3fe6bcb38e733
liaoyinzong <354575573@qq.com>
2018-09-06 17:00:11 +0800

    no message

--- application/admin/view/index/index.php
+++ application/admin/view/index/index.php
@@ -360,7 +360,7 @@
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y} 件</b></td></tr>',
+            '<td style="padding:0"><b>{point.y} 单</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
